7082f8e96d4e677722a0be3f476e48eb51ee1911
TomasMRivero <tmartinezrivero@alumno.unlam.edu.ar>
2022-01-17 14:54:27 -0300

    initial commit

---
+++ src/main/java/com/tomasmartinez/cursobackend/CursoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tomasmartinez.cursobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomasmartinez/cursobackend/CursoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomasmartinez.cursobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
