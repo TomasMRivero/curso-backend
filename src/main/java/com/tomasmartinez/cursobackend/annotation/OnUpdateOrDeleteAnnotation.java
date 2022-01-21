1a3d67dddfb3eccc41a158bdc44a510e95f28179
TomasMRivero <tmartinezrivero@alumno.unlam.edu.ar>
2022-01-20 21:31:08 -0300

    crear anotacion para metodos delete y update

---
+++ src/main/java/com/tomasmartinez/cursobackend/annotation/OnUpdateOrDeleteAnnotation.java
@@ -0,0 +1,9 @@
+package com.tomasmartinez.cursobackend.annotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(value = {ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface OnUpdateOrDeleteAnnotation {
+}
