e0b39686a3ce18336f9cd98544a1076fcbe1b5e6
TomasMRivero <tmartinezrivero@alumno.unlam.edu.ar>
2022-01-25 13:11:51 -0300

    crear repositorio de producto

---
+++ src/main/java/com/tomasmartinez/cursobackend/repository/ProductRepository.java
@@ -0,0 +1,21 @@
+package com.tomasmartinez.cursobackend.repository;
+
+import com.tomasmartinez.cursobackend.models.Product;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface ProductRepository extends CrudRepository<Product, Long> {
+    Product findByName(String name);
+    List<Product> findByStockGreaterThan(int stock);
+    List<Product> findByStockLessThan(int stock);
+
+    @Modifying
+    @Transactional
+    @Query("update Product p set u.stock = :stock where u.name = :name")
+    void updateAgeByName(@Param(value = "stock") int stock, @Param(value="name") String name);
+}
