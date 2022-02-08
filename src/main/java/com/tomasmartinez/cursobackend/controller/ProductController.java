c6261efe3d2099b0f8e7ce9c9511a00bc98dc3a8
TomasMRivero <tmartinezrivero@alumno.unlam.edu.ar>
2022-02-08 14:35:17 -0300

    crear rutas para la api

---
+++ src/main/java/com/tomasmartinez/cursobackend/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.tomasmartinez.cursobackend.controller;
+
+import com.tomasmartinez.cursobackend.model.request.ProductRequest;
+import com.tomasmartinez.cursobackend.model.response.ProductResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/product")
+public class ProductController {
+
+    private final ProductService service;
+
+    @PostMapping("")
+    public ProductResponse createProduct(@Validated @RequestBody ProductRequest product){
+        return service.create(product);
+    }
+
+    @GetMapping("/all")
+    public List<ProductResponse> getProductList(){
+        return service.searchAll();
+    }
+
+}
