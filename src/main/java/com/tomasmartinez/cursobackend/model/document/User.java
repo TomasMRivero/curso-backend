142fd843910c4645618a864d864b565242c6e0c2
TomasMRivero <tmartinezrivero@alumno.unlam.edu.ar>
2022-02-14 21:29:53 -0300

    modificar modelo de usuario

--- src/main/java/com/tomasmartinez/cursobackend/model/document/User.java
+++ src/main/java/com/tomasmartinez/cursobackend/model/document/User.java
@@ -3,23 +3,55 @@ package com.tomasmartinez.cursobackend.model.document;
 import lombok.*;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Id;
+import java.util.Collection;
+import java.util.List;
 
 @Builder
 @Getter
 @Setter
 @ToString
 @AllArgsConstructor
-@NoArgsConstructor
 @Document("user")
-public class User {
-    @Id
-    private String id;
-    @Indexed(unique=true)
-    private String userId;
-    @Indexed(unique=true)
-    private String email;
-    private String password;
-    private String token;
+public class User implements UserDetails {
+    private final List<? extends GrantedAuthority> grantedAuthorities;
+    private final String password;
+    private final String username;
+    private final boolean isAccountNonExpired;
+    private final boolean isAccountNonLocked;
+    private final boolean isCredentialsNonExpired;
+    private final boolean isEnabled;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return grantedAuthorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return isAccountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return isAccountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return isCredentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
+    }
 }
